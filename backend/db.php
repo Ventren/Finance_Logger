6c70b0f9c892b7b8781a1b5cbc31b733a53c5ec8
Ventren Nair <ventrenn18@gmail.com>
2026-01-08 23:40:40 +0800

    DB connection

---
+++ backend/db.php
@@ -0,0 +1,20 @@
+<?php
+header ("Access-Control-Allow-Origin: http://localhost:5173/");
+header("Access-Control-Allow-Methods:GET,POST,PUT,DELETE,OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+$host = "localhost";
+$dbname = "financial_logger";
+$username = "root";
+$pass = "";
+
+try{
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+}catch(PDOException $e){
+    die("Connection Failed: " . $e->getMessages());
+}
+?>
